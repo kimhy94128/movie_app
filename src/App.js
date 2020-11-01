92c37152ee5d1f7fe05b7544eef62d5ce8a817b3
HOOK <kimhy94128@gmail.com>
2020-11-01 23:22:11 +0900

    css fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie"
+import "./App.css"
 
 class App extends React.Component {
   state = {
@@ -18,8 +19,14 @@ class App extends React.Component {
   render(){
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? "로딩..." : movies.map(movie => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">로딩...</span>
+          </div>
+        ) : (
+          <div className="movies">
+          {movies.map(movie => (
           <Movie
           key={movie.id}
           id={movie.id}
@@ -27,9 +34,12 @@ class App extends React.Component {
           title={movie.title}
           summary={movie.summary}
           poster={movie.medium_cover_image}
+          genres={movie.genres}
           />
         ))}
-      </div>
+        </div>
+        )}
+      </section>
     ) 
   }
 }
